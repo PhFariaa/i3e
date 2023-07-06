0458d25e2bc41443e05e797c0cc39997a7d0cf0a
Pedro <pedrinhfm@gmail.com>
2023-07-06 14:22:55 -0300

    mudando o nome das colunas do arquivo

--- imdb.py
+++ imdb.py
@@ -1,4 +1,6 @@
 import pandas as pd
+#import matplotlib as mtp
 
 imdb_df = pd.read_csv("imdb_clean.csv")
-print(imdb_df.head(5))
+
+imdb_df.columns = ["titulo", "ano de lancamento", "duracao", "genero", "nota", "Gross(M)"]
